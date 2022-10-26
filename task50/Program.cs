ef4696c1ca9975d75bca3ea52fb1abee2b1e1052
Ilya Matveev01~ <aorn666@gmail.com>
2022-10-26 19:23:25 +0300

    три задачки на одну сигорету скучно

---
+++ task50/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+void InputMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(-10, 11);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+
+Console.Write("Введите номер строки: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+a--;
+b--;
+if (n>=a && m>=b)
+{
+    Console.Write($"{matrix[a, b]} \t");
+}
+else
+{
+    Console.Write("нет такого элемента массива");
+}
---
+++ task52/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+void InputMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(1, 11);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+void SumMatrix(int[,] matrix)
+{
+    double sum = 0;
+    int a = 0;
+for (int j = 0; j < matrix.GetLength(1);j++)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+sum=sum + matrix[i, j];
+a++;
+}
+Console.Write($"{sum/a} \t");
+sum = 0;
+a =  0;
+}
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.Write("Среднеарефметическое столбцов: ");
+Console.WriteLine();
+SumMatrix(matrix);
